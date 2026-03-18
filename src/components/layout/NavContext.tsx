189c5fa1b90e9fc2f5672027174388dfce1c2b99
REAIAN18 <ian@islcap.com>
2026-03-18 11:50:01 +0000

    feat: add src/components/layout/NavContext.tsx

---
+++ src/components/layout/NavContext.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+interface NavContextType {
+  sidebarOpen: boolean;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+}
+
+const NavContext = createContext<NavContextType>({
+  sidebarOpen: false,
+  openSidebar: () => {},
+  closeSidebar: () => {},
+});
+
+export function useNav() {
+  return useContext(NavContext);
+}
+
+export function NavProvider({ children }: { children: React.ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <NavContext.Provider
+      value={{
+        sidebarOpen,
+        openSidebar: () => setSidebarOpen(true),
+        closeSidebar: () => setSidebarOpen(false),
+      }}
+    >
+      {children}
+    </NavContext.Provider>
+  );
+}
